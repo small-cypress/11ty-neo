64987a9b504cc008bb536598f5d377f5ee255252
small-cypress <mollykempson@gmail.com>
2025-03-09 18:38:52 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addFilter("itemLimit", function(array, maximum) {
+    return array.slice(0, maximum);
+});
   eleventyConfig.addPassthroughCopy("./src/images");
   eleventyConfig.addPassthroughCopy("./src/js");
   return {
